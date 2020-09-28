34718511fff55b6acd03fce66419ce452f03ef1f
pkkwilliam <pkkwilliam919@gmail.com>
2020-09-28 18:06:47 +0800

    Updates

---
+++ precache-manifest.e5354f3ada3d4b5903d1b32dedbd08f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d34a2f493124fda077d5f78f2458cd",
+    "url": "/public_web/index.html"
+  },
+  {
+    "revision": "32d67456638bcb9a2414",
+    "url": "/public_web/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6dcf007925f51a964c04",
+    "url": "/public_web/static/js/2.338bae8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/public_web/static/js/2.338bae8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32d67456638bcb9a2414",
+    "url": "/public_web/static/js/main.6a6efb8b.chunk.js"
+  },
+  {
+    "revision": "a4512cc79eaaf90792a5",
+    "url": "/public_web/static/js/runtime-main.5448edcd.js"
+  },
+  {
+    "revision": "cb7e4eea53382febd0a456c758f6a93a",
+    "url": "/public_web/static/media/campaign_1.cb7e4eea.pdf"
+  },
+  {
+    "revision": "9a423d5346762be6954201223109ae76",
+    "url": "/public_web/static/media/leaderfocus_logo.9a423d53.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/public_web/precache-manifest.9c62047263d2243ded8c2cdebdd136b9.js"
+  "/public_web/precache-manifest.e5354f3ada3d4b5903d1b32dedbd08f7.js"
 );
 
 self.addEventListener('message', (event) => {
