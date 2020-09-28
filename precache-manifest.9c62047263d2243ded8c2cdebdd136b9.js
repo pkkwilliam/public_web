0288f741266c76ab3a6f9e97dfb5b7e59998fb04
pkkwilliam <pkkwilliam919@gmail.com>
2020-09-28 14:56:29 +0800

    Updates

---
+++ precache-manifest.9c62047263d2243ded8c2cdebdd136b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463d48d416184d896b03e53f789ed4ae",
+    "url": "/public_web/index.html"
+  },
+  {
+    "revision": "bf35f5af7d6f66c0cb5e",
+    "url": "/public_web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4162230a1b3b0840d05",
+    "url": "/public_web/static/js/2.da62da10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/public_web/static/js/2.da62da10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf35f5af7d6f66c0cb5e",
+    "url": "/public_web/static/js/main.b0cc03bb.chunk.js"
+  },
+  {
+    "revision": "a4512cc79eaaf90792a5",
+    "url": "/public_web/static/js/runtime-main.5448edcd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/public_web/static/media/logo.5d5d9eef.svg"
+  }
+]);
